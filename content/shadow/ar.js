e054fbd9939c0f94c579a4916bf8aa5f41bf3cdf
NicholiNoah <external@spatialmedialab.com>
2022-10-26 13:27:41 -0500

    images/course-banner.mind

--- content/shadow/ar.js
+++ content/shadow/ar.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // initialize MindAR 
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: '../../assets/targets/course-banner.mind',
+      imageTargetSrc: './images/course-banner.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
