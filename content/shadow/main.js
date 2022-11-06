397e76f5f6768b1e564488d9ad38ddd6c498e357
Nicholi Noah <43762250+NicholiNoah@users.noreply.github.com>
2022-11-06 14:37:08 -0600

    targets

--- content/shadow/main.js
+++ content/shadow/main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // initialize MindAR 
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: './images/SHADOW.mind',
+      imageTargetSrc: './targets/SHADOW.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
