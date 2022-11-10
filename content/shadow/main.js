e8944f69f2bd0114f7e8a2bda915ce05e232af7e
Nicholi Noah <43762250+NicholiNoah@users.noreply.github.com>
2022-11-10 08:25:52 -0600

    connect .js files to spatialmedialab.com host

--- content/shadow/main.js
+++ content/shadow/main.js
@@ -1,22 +1,22 @@
-import {loadGLTF} from "../../libs/loader.js";
+import {loadGLTF} from "http://spatialmedialab.com/libs/loader.js";
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: '../../assets/targets/musicband.mind'
+      imageTargetSrc: 'http://spatialmedialab.com/assets/targets/musicband.mind'
     });
     const {renderer, scene, camera} = mindarThree;
 
     const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
     scene.add(light);
 
-    const raccoon = await loadGLTF('../../assets/models/musicband-raccoon/scene.gltf');
+    const raccoon = await loadGLTF('http://spatialmedialab.com/assets/models/musicband-raccoon/scene.gltf');
     raccoon.scene.scale.set(0.1, 0.1, 0.1);
     raccoon.scene.position.set(0, -0.4, 0);
 
-    const bear = await loadGLTF('../../assets/models/musicband-bear/scene.gltf');
+    const bear = await loadGLTF('http://spatialmedialab.com/assets/models/musicband-bear/scene.gltf');
     bear.scene.scale.set(0.1, 0.1, 0.1);
     bear.scene.position.set(0, -0.4, 0);
 
